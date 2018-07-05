ab8533b599fa939f053c6afbb9a5f773c800e5e9
gitkiselev <482003@mail.ru>
2018-07-05 17:49:24 +0300

    loading blocks waits checking

--- js/script.js
+++ js/script.js
@@ -253,6 +253,23 @@
 			
 		giftModal.addEventListener('click', hidePopupModalGift);
 
+
+
+
+		//Подгрузка блоков
+		let moreBtn = document.querySelector('.button-styles');
+		let showHiddenBlocks = document.querySelectorAll('.styles-2');
+		function showHiddenBlocksFunc(){
+			for(let i = 0; i < showHiddenBlocks.length; i++){
+				let showHiddenBlock = showHiddenBlocks[i];
+				showHiddenBlock.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
+
+			}
+			moreBtn.style.display = 'none';
+		}
+		moreBtn.addEventListener('click', showHiddenBlocksFunc);
+		
+
 	
 
 
