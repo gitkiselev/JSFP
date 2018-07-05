09f94f0b8e773413fdb3d4deb15251d17ab50e1e
gitkiselev <482003@mail.ru>
2018-07-05 17:25:35 +0300

    gift modal waits checking

--- js/script.js
+++ js/script.js
@@ -229,9 +229,29 @@
 		    formDFInput[i].value = ""; // ощищаем поля ввода
 		  }
 		});
+		//modal gift
+		let gift = document.querySelector('.fixed-gift');
+		let giftModal = document.querySelector('.popup-gift');
+		gift.addEventListener('click', showGiftModal);
+		function showGiftModal(){
+			giftModal.style.display = 'block';
+			document.body.overflow = 'hidden';
+			gift.style.display = 'none';
+		}
+		function hidePopupModalGift(e){
+			console.log('hide design or consultation');
+			if(e.target.classList.contains('popup-gift') || e.target.classList.contains('popup-close')){
+				giftModal.style.display = 'none';
+				document.body.style.overflow = '';
+			}			
+		}
 
+		for(let i = 0; i < btnsPopupClose.length; i++){
+			let btnPopupClose = btnsPopupClose[i];
+			btnPopupClose.addEventListener('click', hidePopupModalGift);
+		}
 			
-
+		giftModal.addEventListener('click', hidePopupModalGift);
 
 	
 
