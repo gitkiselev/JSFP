d19246081c637a894a4756f62ff6f2b663368d7a
gitkiselev <482003@mail.ru>
2018-07-06 20:06:30 +0300

    showing and hiding pictures and mobule events waits checking

--- js/script.js
+++ js/script.js
@@ -443,6 +443,58 @@ function isPromo(){
 	  	}
     }
   });//Фильтрация блоков конец
+
+
+
+  //Картинки при наведении
+
+  let sizesBlock = document.querySelectorAll('.sizes-block');
+  let sw = document.querySelector('.sizes-wrapper');//общий контейнер
+  let imgs = sw.querySelectorAll('img');
+  
+  
+  	
+  
+	sw.addEventListener('mouseover', showImages);
+  	sw.addEventListener('mouseout', hideImages);
+    sw.addEventListener('tap', showImages);
+  	sw.addEventListener('tap', hideImagesM);
+  	
+  
+  	
+  
+  function showImages(e){
+
+  	
+  		
+	  	if(e.target.tagName == 'IMG'){
+	  		e.target.style.position = 'relative';
+	    	e.target.style.zIndex = '10';
+	    	e.target.src = e.target.src.replace('.png', '-1.png');
+	  	}
+	    
+  	
+ 
+  	
+
+  }
+
+  function hideImages(e){
+  	if(e.target.tagName == 'IMG'){
+  		e.target.style.position = '';
+    	e.target.style.zIndex = '';
+    	e.target.src = e.target.src.replace('-1.png', '.png');
+  	}
+  }
+  function hideImagesM(e){
+  	if(e.target.tagName != 'IMG'){
+  		e.target.style.position = '';
+    	e.target.style.zIndex = '';
+    	e.target.src = e.target.src.replace('-1.png', '.png');
+  	}
+  }
+  
+  
   
 		
 
